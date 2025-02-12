03c5e8a6b26e4604e86ca53a3260985560f45175
Apoorv Mittal <apoorvmittal9@gmail.com>
2025-02-12 23:31:31 +0530

    Delete judging_service directory

--- judging_service/config.py
+++
@@ -1,3 +0,0 @@
-PORT = 5001
-MATCH_THRESHOLD = 0.85  # Similarity threshold for a match
-WANTED_DB_PATH = "wanted_faces.pkl"  # Stores wanted face encodings
--- judging_service/run_judge.py
+++
@@ -1,4 +0,0 @@
-from judge import app
-
-if __name__ == "__main__":
-    app.run()
